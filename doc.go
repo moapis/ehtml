06b6316b27b9b9c70856f9c5ca5e83c217340504
Tim Möhlmann <muhlemmer@gmail.com>
2020-04-20 17:59:38 +0300

    Refactor: use an interface type for data

--- doc.go
+++ doc.go
@@ -19,12 +19,12 @@ Define some templates:
 	{{- define "error" -}}
 	<!DOCTYPE html>
 	<html lang="en">
-	{{ template "head" }}
+	{{ template "head" . }}
 	<body>
-		<h1>{{ .StatusCode }} {{ .StatusText }}</h1>
+		<h1>{{ .Status.Int }} {{ .Status }}</h1>
 		<p>
 			{{ .Message }} while serving {{ .Request.URL.Path }}.
-			Request ID: {{ .Data.RequestID }}
+			Request ID: {{ .ReqID }}
 		</p>
 		<p><i>This is a generic error page</i><p>
 	</body>
@@ -34,17 +34,17 @@ Define some templates:
 	{{- define "500" -}}
 	<!DOCTYPE html>
 	<html lang="en">
-	{{ template "head" }}
+	{{ template "head" . }}
 	<body>
 		<h1>Snap!</h1>
-		<h2>{{ .StatusCode }} {{ .StatusText }}</h2>
+		<h2>{{ .Status.Int }} {{ .Status }}</h2>
 		<p>
 			Something went really wrong and we've been notified!
 			Please try again later.
 		</p>
 		<p><i>
 			Error: {{ .Message }} while serving {{ .Request.URL.Path }}.
-			Request ID: {{ .Data.RequestID }}
+			Request ID: {{ .ReqID }}
 		</i></p>
 	</body>
 	</html>
@@ -54,12 +54,13 @@ Define some templates:
 	<!DOCTYPE html>
 	<html lang="en">
 	<head>
-		{{ template "head" }}
+		{{ template "head" . }}
 	</head>
 	<body>
-		<h1>{{ .StatusCode }} {{ .StatusText }}</h1>
+		<h1>{{ .Status.Int}} {{ .Status }}</h1>
 		<p>
 			{{ .Request.URL.Path }} could not be found.
+		</p>
 	</body>
 	</html>
 	{{- end -}}
@@ -70,16 +71,24 @@ Parse them into a globale variable (or part of your Handler object):
 
 If you are using Gorilla mux, set the `NotFoundHandler`
 
-	r := mux.NewRouter()
-	r.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if err := p.Render(w, r, http.StatusNotFound, "", nil); err != nil {
+	rtr := mux.NewRouter()
+	rtr.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if err := p.Render(w, &Data{Req: r, Code: http.StatusNotFound}); err != nil {
 			log.Println(err)
 		}
 	})
 
+Optionally, extend `Data` to add more context.
+As an alternative, you can also roll your own implementation of `Provider`.
+
+	type data struct {
+		Data
+		ReqID int
+	}
+
 And whenever something goes wrong in your handlers, call `Render()`:
 
-	err := p.Render(w, req, http.StatusInternalServerError, "DB connection", struct{ RequestID int }{666})
+	err := p.Render(w, &data{Data{req, http.StatusInternalServerError, "DB connection"}, 666})
 	if err != nil {
 		log.Println(err)
 	}
